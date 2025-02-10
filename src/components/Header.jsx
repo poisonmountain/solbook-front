ec54194efe83e613b8f055b51284700ccd7ebae5
ghktndyd <hwasuyong99@naver.com>
2025-02-10 14:17:54 +0900

    헤더 디자인 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import "../styles/Header.css";
+import '../styles/Header.css';
 
 export default function Header({ logo }) {
   const navigate = useNavigate();
 
   return (
     <header className="Header">
-      <div className="logo-container" onClick={() => navigate('/')}>
-        {logo ? <img src={logo} alt="Logo" className="logo-image" /> : null}
-        <span className="logo-text">SolBook</span>
+      <div className="header_left">
+        <div className="logo-container" onClick={() => navigate('/')}>
+          {logo ? <img src={logo} alt="Logo" className="logo-image" /> : null}
+          <span className="logo-text">SolBook</span>
+        </div>
       </div>
+      <div className="header_center">dd</div>
+      <div className="header_right">dd</div>
     </header>
   );
 }
